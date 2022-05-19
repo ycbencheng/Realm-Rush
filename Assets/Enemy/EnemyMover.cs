e3b32448346337bf4120da0e0baabc15d9fe49c2
ycbencheng <ycbencheng@gmail.com>
2022-05-19 13:04:57 -0600

    Organize folders

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour {
+
+  [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    PrintWaypointsName();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void PrintWaypointsName() {
+    foreach(Waypoint waypoint in path) {
+      Debug.Log(waypoint.name);
+    }
+  }
+}
