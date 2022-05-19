c13ac0c827742e05a5aceaeea75ec20720320c13
ycbencheng <ycbencheng@gmail.com>
2022-05-19 12:17:42 -0600

    Create logic to display coordinates on the fly

---
+++ Assets/CoordinateLabeler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+
+public class CoordinateLabeler:MonoBehaviour {
+  TextMeshPro label;
+  Vector2Int coordinates = new Vector2Int();
+
+  // Start is called before the first frame update
+  void Awake() {
+    label = GetComponent<TextMeshPro>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if(!Application.isPlaying) {
+      DisplayCoordinates();
+      UpdateObjectName();
+    }
+  }
+
+  void DisplayCoordinates() {
+    coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+    coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+    label.text = coordinates.x + "," + coordinates.y;
+  }
+
+  void UpdateObjectName() {
+    transform.parent.name = coordinates.ToString();
+  }
+}
