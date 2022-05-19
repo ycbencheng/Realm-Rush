e1ec8cbc721a528db694695a74dcc0f7d4258da8
ycbencheng <ycbencheng@gmail.com>
2022-05-19 16:12:51 -0600

    Use Lerp to movment enemy

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour {
 
   [SerializeField] List<Waypoint> path = new List<Waypoint>();
-  [SerializeField] float waitTime = 1f;
+  [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
-  // Start is called before the first frame update
-  void Start()
-  {
+  void Start() {
     StartCoroutine(FollowPath());
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
   }
 
   IEnumerator FollowPath() {
     foreach(Waypoint waypoint in path) {
-      transform.position = waypoint.transform.position;
+      Vector3 startPosition = transform.position;
+      Vector3 endPosition = waypoint.transform.position;
+      float travelPercent = 0f;
 
-      yield return new WaitForSeconds(waitTime);
+      transform.LookAt(endPosition);
+
+      while(travelPercent < 1f) {
+        travelPercent += Time.deltaTime * speed;
+        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+
+        yield return new WaitForEndOfFrame();
+      }
     }
   }
 }
