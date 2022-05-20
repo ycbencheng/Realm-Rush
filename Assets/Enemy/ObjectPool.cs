0e975d3f71d8be80d90c62f06900f32d8c5530d5
ycbencheng <ycbencheng@gmail.com>
2022-05-20 14:21:50 -0600

    Instantiate enemy on the fly

---
+++ Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+  [SerializeField] GameObject enemyPrefab;
+  [SerializeField] float spawnTimer = 1f;
+
+  // Start is called before the first frame update
+  void Start() {
+    StartCoroutine(SpawnEnemy());
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  IEnumerator SpawnEnemy() {
+    while(true) {
+      Instantiate(enemyPrefab, transform);
+      yield return new WaitForSeconds(spawnTimer);
+    }
+  }
+}
